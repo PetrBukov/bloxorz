6e61fbe014d0391452d8428dbd4951aa24dbd3c0
Petr Bukov <pbu.vsl@gmail.com>
2023-07-20 14:38:52 +0400

    feat: setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 const App: React.FC = () => {
-  return (
-    <div>
-      Hello Bloxorz
-    </div>
-  );
+  return <div>Hello Bloxorz</div>
 }
 
-export default App;
+export default App
