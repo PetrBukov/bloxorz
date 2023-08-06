d8a0febe9d039211f334ddbc01da74c9c194f2dd
Petr Bukov <pbu.vsl@gmail.com>
2023-08-06 16:36:07 +0400

    fix: failure status message in tutorial 1

--- src/components/GameStatusTitle/GameStatusTitle.utils.ts
+++ src/components/GameStatusTitle/GameStatusTitle.utils.ts
@@ -3,33 +3,37 @@ import { getLevelById, getFullLevelName } from '../../utils'
 import { StatusTextVariant } from './GameStatusTitle.types'
 
 export const getStatusText = (levelId: LevelID, activeAction: GameBoardAction): Array<string> => {
-  if (levelId === 'tutorial-1') {
-    return ["Let's solve", 'Them all 😎']
-  }
-
   switch (activeAction.type) {
     case GameBoardActionType.heroBlockOutOfMap:
     case GameBoardActionType.playerHasNoMoves: {
       return ['Oooops ...', 'Try again!']
     }
+
     case GameBoardActionType.levelCompleted: {
+      if (levelId === 'tutorial-1') {
+        return ["Let's solve", 'Them all 😎']
+      }
+
       const currentLevel = getLevelById(levelId)
       const currentLevelFullName = getFullLevelName(currentLevel?.name, currentLevel?.type)
 
       return [currentLevelFullName, 'Completed']
     }
+
     case GameBoardActionType.moveToAnotherLevel: {
       const nextLevel = getLevelById(activeAction.levelId)
       const nextLevelFullName = getFullLevelName(nextLevel?.name, nextLevel?.type)
 
       return ['Moving to', nextLevelFullName]
     }
+
     case GameBoardActionType.moveToAnotherStage: {
       const nextStage = getLevelById(activeAction.stageId)
       const nextStageFullName = getFullLevelName(nextStage?.name, nextStage?.type)
 
       return ['Moving to', nextStageFullName]
     }
+
     default:
       return []
   }
@@ -39,18 +43,20 @@ export const getStatusTextVariant = (
   levelId: LevelID,
   activeAction: GameBoardAction,
 ): StatusTextVariant => {
-  if (levelId === 'tutorial-1') {
-    return StatusTextVariant.info
-  }
-
   switch (activeAction.type) {
     case GameBoardActionType.heroBlockOutOfMap:
     case GameBoardActionType.playerHasNoMoves: {
       return StatusTextVariant.failure
     }
+
     case GameBoardActionType.levelCompleted: {
+      if (levelId === 'tutorial-1') {
+        return StatusTextVariant.info
+      }
+
       return StatusTextVariant.success
     }
+
     case GameBoardActionType.moveToAnotherLevel:
     case GameBoardActionType.moveToAnotherStage:
     default:
