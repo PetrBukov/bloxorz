14490e6b87e2657dc3e1cf89e501798e323ef511
Petr Bukov <pbu.vsl@gmail.com>
2023-08-08 18:37:01 +0400

    feat: improve tips for tutorial 1

--- src/components/TutorialTips/tips/TutorialTips1/TutorialTips1.styles.ts
+++ src/components/TutorialTips/tips/TutorialTips1/TutorialTips1.styles.ts
@@ -1,34 +1,122 @@
 import styled from '@emotion/styled'
+import { keyframes } from '@emotion/react'
 
 import { GAP_BETWEEN_TILES, TILE_WIDTH } from '../../../../constants'
+
+const swipeAnimation = keyframes`
+  0% {
+    opacity: 10%;
+    left: -40px;
+  }
+  100% {
+    opacity: 90%;
+    left: 40px;
+  }
+`
+
+const keyPressAnimation = keyframes`
+  0% {
+    opacity: 70%;
+    top: 0px;
+    box-shadow: 0px 2px var(--gray400);
+  }
+  100% {
+    opacity: 100%;
+    top: 2px;
+    box-shadow: 0px 0px var(--gray400);
+  }
+`
 
 export const TutorialContainer = styled.div`
   position: relative;
   width: ${TILE_WIDTH * 5 + GAP_BETWEEN_TILES * 4}px;
-  height: auto;
+  height: 500px;
 
-  display: grid;
-  grid-template-rows: repeat(6, 60px);
-  grid-gap: 16px;
-
-  div {
+  > div {
+    position: absolute;
     color: var(--white);
     font-size: 16px;
     line-height: 24px;
     text-align: center;
+    width: 100%;
 
     span {
       color: var(--acidGreen);
     }
   }
 
-  div:nth-of-type(2) {
+  > div:nth-of-type(2) {
     font-family: simpleStamp, sans-serif;
     font-size: 56px;
     color: var(--acidGreen);
   }
 
-  div:nth-of-type(3) {
-    grid-row: 5;
+  > div:first-of-type {
+    top: 80px;
+  }
+
+  > div:nth-of-type(2) {
+    top: 130px;
+  }
+
+  > div:nth-of-type(3) {
+    top: 330px;
+  }
+
+  > div:nth-of-type(4) {
+    top: 430px;
+  }
+`
+
+export const ControllersTipsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 20px 1fr;
+
+  grid-row: 5;
+
+  justify-items: center;
+  align-items: center;
+`
+
+export const SwipeIconContainer = styled.div`
+  position: relative;
+  animation: ${swipeAnimation} 3.5s Ease-out infinite;
+`
+
+export const KeyboardArrowsContainer = styled.div`
+  display: grid;
+  width: ${(TILE_WIDTH / 2) * 3 + 8}px;
+  height: ${(TILE_WIDTH / 2) * 2 + 4}px;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+  gap: 4px;
+
+  > div {
+    display: grid;
+    align-items: center;
+    justify-items: center;
+
+    background-color: var(--white);
+    box-shadow: 0px 2px var(--gray400);
+    border-radius: 2px;
+    opacity: 70%;
+
+    svg {
+      color: var(--gray500);
+      font-size: 20px;
+    }
+  }
+
+  > div:first-of-type {
+    grid-column: 2;
+  }
+
+  > div:nth-of-type(2) {
+    grid-column: 1;
+  }
+
+  > div:last-of-type {
+    position: relative;
+    animation: ${keyPressAnimation} 2s steps(2) infinite;
   }
 `
--- src/components/TutorialTips/tips/TutorialTips1/TutorialTips1.tsx
+++ src/components/TutorialTips/tips/TutorialTips1/TutorialTips1.tsx
@@ -1,10 +1,43 @@
-import { TutorialContainer } from './TutorialTips1.styles'
+import {
+  PiHandSwipeRightDuotone,
+  PiArrowUpBold,
+  PiArrowRightBold,
+  PiArrowDownBold,
+  PiArrowLeftBold,
+} from 'react-icons/pi'
+
+import {
+  ControllersTipsContainer,
+  KeyboardArrowsContainer,
+  SwipeIconContainer,
+  TutorialContainer,
+} from './TutorialTips1.styles'
 
 export const TutorialTips1: React.FC = () => {
   return (
     <TutorialContainer>
       <div>welcome to the ...</div>
       <div>Puzzle Slime</div>
+      <ControllersTipsContainer>
+        <KeyboardArrowsContainer>
+          <div>
+            <PiArrowUpBold />
+          </div>
+          <div>
+            <PiArrowLeftBold />
+          </div>
+          <div>
+            <PiArrowDownBold />
+          </div>
+          <div>
+            <PiArrowRightBold />
+          </div>
+        </KeyboardArrowsContainer>
+        <div>OR</div>
+        <SwipeIconContainer>
+          <PiHandSwipeRightDuotone size={50} />
+        </SwipeIconContainer>
+      </ControllersTipsContainer>
       <div>
         <span>use the arrow buttons</span> on your keyboard <span>or swipe on your screen</span> to
         move the Slime to the tile with a round hole
