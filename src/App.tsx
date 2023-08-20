5405f52f40556d539cf4e8ae2dc4765b81cb74a4
Petr Bukov <pbu.vsl@gmail.com>
2023-08-20 16:32:05 +0400

    fix: remove global styles rerender

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { GameCenter } from './providers'
 import { Content, Footer, Header, Navigation } from './components'
 import { WelcomeScreen } from './components/WelcomeScreen/WelcomeScreen'
 
-const App: React.FC = () => {
+const AppContent: React.FC = () => {
   const [loadingGameData, setLoadingGameData] = useState<boolean>(true)
 
   useEffect(() => {
@@ -20,20 +20,25 @@ const App: React.FC = () => {
     return () => clearTimeout(timeout)
   }, [])
 
+  if (loadingGameData) {
+    return <WelcomeScreen />
+  }
+
+  return (
+    <GameCenter>
+      <Header />
+      <Content />
+      <Footer />
+      <Navigation />
+    </GameCenter>
+  )
+}
+
+const App: React.FC = () => {
   return (
     <React.Fragment>
       <Global styles={globalStyles} />
-
-      {loadingGameData ? (
-        <WelcomeScreen />
-      ) : (
-        <GameCenter>
-          <Header />
-          <Content />
-          <Footer />
-          <Navigation />
-        </GameCenter>
-      )}
+      <AppContent />
     </React.Fragment>
   )
 }
