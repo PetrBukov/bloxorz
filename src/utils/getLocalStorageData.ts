ebff6f70505452a7f461ff75a8c57b515b4604f1
Petr Bukov <pbu.vsl@gmail.com>
2023-09-17 14:54:03 +0400

    fix: add error handling for localStorage parsing

--- src/utils/getLocalStorageData.ts
+++ src/utils/getLocalStorageData.ts
@@ -4,13 +4,26 @@ import { AppLocalStorageData, AppLocalStorage } from '../types/localStorage'
 export const getLocalStorageData = (): AppLocalStorageData => {
   const stringifiedAppLocalStorage = localStorage.getItem(LOCAL_STORAGE_ITEM_NAME)
 
-  if (stringifiedAppLocalStorage) {
-    const appLocalStorage = JSON.parse(stringifiedAppLocalStorage) as AppLocalStorage
+  try {
+    if (stringifiedAppLocalStorage) {
+      const appLocalStorage = JSON.parse(stringifiedAppLocalStorage) as AppLocalStorage
 
-    // TODO: Add migrations handling here to avoid losing player progress if version of localStorage was changed
-    if (appLocalStorage.version === LOCAL_STORAGE_VERSION) {
-      return appLocalStorage.data
+      // If there is no appLocalStorage.data or appLocalStorage.data.lastCompletedLevel
+      // or lastCompletedLevel has incorrect type we throw an Error
+      if (typeof appLocalStorage?.data?.lastCompletedLevel !== 'number') {
+        throw new Error('Incorrect lastCompletedLevel property')
+      }
+
+      // TODO: Add migrations handling here to avoid losing player progress if version of localStorage was changed
+      if (appLocalStorage.version === LOCAL_STORAGE_VERSION) {
+        return appLocalStorage.data
+      }
     }
+  } catch (e) {
+    // We don't have any recovery functionality so if game data in a browser localStorage has errors
+    // we just write about it in the console (so someone could fix data manually)
+    // and allow our script to continue working as if there is no data at all in the localStorage
+    console.error(e)
   }
 
   return {
