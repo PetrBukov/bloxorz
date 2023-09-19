c3c21ae47c2a548cdd58c206b72f0a204a4ef926
Petr Bukov <pbu.vsl@gmail.com>
2023-09-19 13:18:20 +0400

    fix: wrong outofmap behaviour

--- src/providers/GameCenter/utils/checkForHeroBlockOutOfMap.ts
+++ src/providers/GameCenter/utils/checkForHeroBlockOutOfMap.ts
@@ -14,6 +14,16 @@ export const checkForHeroBlockOutOfMap = ({
   let isHeroBlockOutOfMap = false
 
   for (let coordinates of heroPlacement) {
+    if (
+      coordinates.x >= gameBoardSize.width ||
+      coordinates.x < 0 ||
+      coordinates.y >= gameBoardSize.height ||
+      coordinates.y < 0
+    ) {
+      isHeroBlockOutOfMap = true
+      break
+    }
+
     const tileIndex = calcTileIndex({ coordinates, gameBoardSize })
     const tile = gameBoardTiles[tileIndex]
 
